8aebe343d116be550d7017254130d99024302498
lnikol <lazar.nikolic@itcentar.rs>
2024-12-11 09:57:12 +0100

    Added table for data view.

--- public/index.php
+++ public/index.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $search = $_GET['search'] ?? '';
 
 $users = Capsule::table('users')
-                ->when($search, fn ($query, $search) => $query->where('email', 'like', "%{$search}%"))
+                ->when(
+                        $search,
+                    fn ($query, $search) => $query
+                        ->where('email', 'like', "%{$search}%")
+                        ->orWhere('name', 'like', "%{$search}%")
+                )
                 ->limit(10)
                 ->get()
                 ->toArray();
@@ -22,24 +27,35 @@ $users = Capsule::table('users')
     <link href="./css/output.css" rel="stylesheet">
     <title>User List</title>
 </head>
-<body class="bg-gray-500">
+<body class="bg-gray-300">
 <div class="m-12">
     <form action="">
         <div class="flex flex-col max-w-48">
-            <input class="p-1" type="text" id="search" name="search">
+            <input class="p-1" type="text" id="search" name="search" value="<?php echo $_GET['search'] ?>">
 
             <input type="submit" value="Search" class="bg-blue-600 hover:bg-blue-900">
         </div>
     </form>
 
     <div class="mt-3">
-        <ul>
+        <table class="table-auto">
+            <thead>
+            <tr class="bg-gray-500">
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created at</th>
+            </tr>
+            </thead>
+            <tbody>
             <?php foreach ($users as $user): ?>
-                <li>
-                    <?php echo $user->name; ?> - <?php echo $user->email; ?>
-                </li>
+                <tr class="bg-gray-200">
+                    <td class="p-1"><?php echo $user->name; ?></td>
+                    <td class="p-1"><?php echo $user->email; ?></td>
+                    <td class="p-1"><?php echo $user->created_at; ?></td>
+                </tr>
             <?php endforeach; ?>
-        </ul>
+            </tbody>
+        </table>
     </div>
 </div>
 </body>
